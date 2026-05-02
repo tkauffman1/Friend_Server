5596f931082c960999e2fa0fc6cb15beebd61a3a
Tyler <tk.tylerkauffman@gmail.com>
2026-05-02 11:22:24 +0000

    updates -

--- MySQL.bat
+++ MySQL.bat
@@ -7,7 +7,7 @@ echo MySQL is currently running. Please only close this window for shutdown.
 echo Please disregard any InoDB messages that are prompted. They have no use.
 echo After your server is shut off, press CTRL C to shut down this service.
 echo __________________________________________________________________________
-mysql\bin\mysqld --standalone --console --max_allowed_packet=128M --innodb_redo_log_capacity=100MB --binlog_expire_logs_seconds=432000 --innodb_buffer_pool_size=2G --innodb_io_capacity=500 --innodb_io_capacity_max=2500 --innodb_use_fdatasync=ON --innodb_buffer_pool_instances=12 --innodb_log_buffer_size=32M --transaction_isolation="READ-COMMITTED"
+mysql\bin\mysqld --standalone --console --max_allowed_packet=128M --innodb_redo_log_capacity=200MB --binlog_expire_logs_seconds=432000 --innodb_buffer_pool_size=2G --innodb_io_capacity=500 --innodb_io_capacity_max=2500 --innodb_use_fdatasync=ON --innodb_buffer_pool_instances=12 --innodb_log_buffer_size=32M --transaction_isolation="READ-COMMITTED"
 
 if errorlevel 1 goto error
 goto finish
